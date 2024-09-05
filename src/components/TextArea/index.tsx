4ef54f99c57808bb0483ec697ec34c45cc128c40
KarlaJulyana <karla@solarz.com.br>
2024-09-04 22:14:00 -0300

    chore: create textarea component

---
+++ src/components/TextArea/index.tsx
@@ -0,0 +1,8 @@
+import { TextArea } from "./text-area";
+
+
+export function TextAreaas() {
+    return (
+        <TextArea placeholder="Ex: Take one tablet twice daily with meals" />
+    )
+}
---
+++ src/components/TextArea/text-area.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { TextArea as RadixTextArea } from "@radix-ui/themes";
+
+export const TextArea = styled(RadixTextArea)`
+    width: 563px;
+    height: 168px;
+    border-radius: 12px;
+    padding: 12px;
+`;
