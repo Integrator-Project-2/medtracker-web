855fd442ec7d625ae10581961aa04056309bd061
KarlaJulyana <karla@solarz.com.br>
2024-09-04 22:41:56 -0300

    refact: make modal dynamic to reuse it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ import { Button } from "@/components/Button";
 import { Modal } from "@/components/Modal";
 import { useState } from "react";
 import { LayoutContainer } from "@/components/LayoutContainer";
+import { Flex } from "@radix-ui/themes";
+import { TextInput } from "@/components/TextInput";
+import { UserCard } from "@/components/UserCard";
 
 export default function Home() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -13,10 +16,35 @@ export default function Home() {
   return (
     <LayoutContainer>
       <Title title="Patients">
-        <Modal 
-          modalOpen={modalOpen}
-          setModalOpen={setModalOpen}   
-        />
+      <Modal 
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+        title="Vinculate new Patient"
+        description="Enter a valid CPF number to search for a new patient"
+        onSubmit={() => console.log("Submit clicked")}
+        onCancel={() => setModalOpen(false)}
+      >
+        <Flex direction="row" gap="3">
+          <TextInput 
+            width="50%"
+            placeholder="000.000.000-00"
+            // value={cpf}
+            // onChange={(e) => setCpf(e.target.value)}
+          />
+          <Button 
+            icon="search-icon.svg"
+            color="var(--navy)"
+            backgroundColor="var(--light-navy)"
+            padding="10px"
+            // onClick={handleSearch}
+          />
+        </Flex>
+
+        <Flex direction="column">
+          <UserCard />
+          <UserCard />
+        </Flex>
+      </Modal>
         <Button 
           icon="plus-icon.svg"
           padding="13px 20px"
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,59 +1,43 @@
-import { Dialog, Flex, TextField } from "@radix-ui/themes";
+import { Dialog, Flex } from "@radix-ui/themes";
 import { StyledDialogContent, StyledDialogDescription, StyledDialogTitle } from "./modal";
 import { Button } from "../Button";
-import { UserCard } from "../UserCard";
-import { TextInput } from "../TextInput";
 
 interface ModalProps {
-    modalOpen: boolean;
-    setModalOpen: (value: boolean) => void;
+  modalOpen: boolean;
+  setModalOpen: (value: boolean) => void;
+  title: string;
+  description: string;
+  children: React.ReactNode;
+  onSubmit: () => void;
+  onCancel?: () => void;
 }
 
-export function Modal({ modalOpen, setModalOpen }: ModalProps) {
+
+export function Modal({ modalOpen, setModalOpen, title, description, children, onSubmit, onCancel }: ModalProps) {
   return (
     <Dialog.Root open={modalOpen}>
       <StyledDialogContent>
-        <StyledDialogTitle>Vinculate new Patient</StyledDialogTitle>
-        <StyledDialogDescription size="2">
-            Enter a valid CPF number to search for a new patient
-        </StyledDialogDescription>
+        <StyledDialogTitle>{title}</StyledDialogTitle>
+        <StyledDialogDescription size="2">{description}</StyledDialogDescription>
 
-        <Flex direction="row" gap="3">
-            <TextInput 
-              width="50%"
-              placeholder="000.000.000-00"
-            />
-
-            <Button 
-              icon="search-icon.svg"
-              color="var(--navy)"
-              backgroundColor="var(--light-navy)"
-              padding="10px"
-            />          
+        <Flex direction="column" gap="3">
+          {children}
         </Flex>
-
-        <Flex direction="column">
-          <UserCard />
-          <UserCard />
-
-        </Flex>
-
 
         <Flex gap="3" mt="4" justify="end">
-            <Button 
-                text="Cancel"
-                padding="14px 41px"
-                backgroundColor="var(--light-navy)"
-                color="var(--navy)"
-                onClick={() => setModalOpen(!modalOpen)}
-            />
+          <Button 
+            text="Cancel"
+            padding="14px 41px"
+            backgroundColor="var(--light-navy)"
+            color="var(--navy)"
+            onClick={onCancel || (() => setModalOpen(false))} 
+          />
 
-            <Button 
-                text="Done"
-                padding="14px 41px"
-                onClick={() => setModalOpen(!modalOpen)}
-            />
-
+          <Button 
+            text="Done"
+            padding="14px 41px"
+            onClick={onSubmit} 
+          />
         </Flex>
       </StyledDialogContent>
     </Dialog.Root>
