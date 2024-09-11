7d226346e55a81a17bb415781687e55a99e4a4bd
KarlaJulyana <karla@solarz.com.br>
2024-09-10 22:30:58 -0300

    chore: update MedicationFormValues

--- src/@types/form-values/MedicationFormValues.ts
+++ src/@types/form-values/MedicationFormValues.ts
@@ -1,4 +1,4 @@
 export interface MedicationFormValues {
     name: string;
-    pharmaceutical_form: string;
+    pharmaceutical_form: 'tablet' | 'capsule' | 'solution' | 'liquid' | 'drops' | 'injectable' | '';
 }
